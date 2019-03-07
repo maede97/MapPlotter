c62c4d59a3dba96445b55cfa6fe34b72fb59c08b
Matthias Busenhart <busenham@student.ethz.ch>
2019-03-07 10:28:31 +0100

    Updates

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -3,17 +3,19 @@
 #include <vector>
 #include <mgl2/mgl.h>
 
-void print(double* p, int n) {
-    for(int i = 0; i < n; i++) {
-        std::cout << p[i] << std::endl;
-    }
-}
-
 int main(int argc, char const *argv[])
 {
     // filename is first argument
-    auto filename = argv[1];
-    int FRAME_COUNT = atoi(argv[2]);
+    if(argc < 3) {
+        std::cerr << "Not enough arguments provided." << std::endl;
+        std::cout << "Usage: " << std::endl << argv[0] << " INPUT FRAMES [OUTPUT]" << std::endl;
+        return 1;
+    }
+
+    const char* OUTPUT_NAME = (argc > 3) ? argv[3] : "output.gif";
+
+    const char* filename = argv[1];
+    const int FRAME_COUNT = atoi(argv[2]);
 
     std::cout << "Reading " << filename << std::endl;
     std::ifstream stream(filename);
@@ -52,25 +54,15 @@ int main(int argc, char const *argv[])
         zp[i] = z;
     }
     std::cout << "Read " << n*m << " lines successfully." << std::endl;
-    /*
-    // DEBUGGING CODE HERE
-    std::cout << "XP" << std::endl;
-    print(xp, m*n);
-    std::cout << "YP" << std::endl;
-    print(yp, m*n);
-    std::cout << "ZP" << std::endl;
-    print(zp, m*n);
-    std::cout << "Coords" << std::endl;
-    print(coords, 2*m*n);
-    */
 
     // plot data
     mglGraph gr;
 
     gr.SetSize(1000,1000);
 
-    gr.StartGIF("output.gif");
+    gr.StartGIF(OUTPUT_NAME);
 
+    // create mglData from double*, length are m*n
     mglData uu(m*n, xp);
     mglData vv(m*n, yp);
     mglData ww(m*n, zp);
@@ -85,9 +77,12 @@ int main(int argc, char const *argv[])
 
         gr.SetRanges(minx, maxx, miny, maxy, minz, maxz);
         //rotate around:  X Z Y
-        gr.Rotate(45, i, 0);
-        gr.Aspect(1,1,1); // set axis equal aspect
-        gr.Axis();
+        gr.Rotate(45, i+30, 0);
+        gr.Aspect(maxx-minx, maxy-miny, (maxz-minz)*5); // set axis equal aspect
+        gr.Axis("+!");
+        gr.Label('x',"Ost-West",0);
+        gr.Label('y',"Nord-Süd",1);
+        gr.Label('z',"Höhe ü. M.",2);
         gr.TriPlot(tr,uu,vv,ww,"b");
         gr.TriPlot(tr,uu,vv,ww,"k#");
         //gr.TriCont(tr,uu,vv,ww,"B"); // contour lines
@@ -99,6 +94,7 @@ int main(int argc, char const *argv[])
 
     gr.CloseGIF();
 
+    // delete dynamically allocated arrays
     delete[] xp;
     delete[] yp;
     delete[] zp;
